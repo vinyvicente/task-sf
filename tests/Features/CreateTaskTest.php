4aaff2b65ae23a735b42a25c1d659772f5f1f900
Vinicius Vicente <vinyvicente@gmail.com>
2020-02-20 17:45:27 -0300

    fix code format

--- tests/Features/CreateTaskTest.php
+++ tests/Features/CreateTaskTest.php
@@ -8,30 +8,42 @@ class CreateTaskTest extends FeatureTestCase
 {
     public function testCreateTask()
     {
-        $this->client->request('POST', '/api/tasks', [],
+        $this->client->request(
+            'POST',
+            '/api/tasks',
+            [],
             [],
             ['CONTENT_TYPE' => 'application/json'],
-            '{"title":"Fabien make a framework, to new version"}');
+            '{"title":"Fabien make a framework, to new version"}'
+        );
 
         $this->assertEquals(201, $this->client->getResponse()->getStatusCode());
     }
 
     public function testCreateTaskWithValidation()
     {
-        $this->client->request('POST', '/api/tasks', [],
+        $this->client->request(
+            'POST',
+            '/api/tasks',
+            [],
             [],
             ['CONTENT_TYPE' => 'application/json'],
-            '{"title":"Fab"}');
+            '{"title":"Fab"}'
+        );
 
         $this->assertEquals(400, $this->client->getResponse()->getStatusCode());
     }
 
     public function testCreateTaskWithEmptyJson()
     {
-        $this->client->request('POST', '/api/tasks', [],
+        $this->client->request(
+            'POST',
+            '/api/tasks',
+            [],
             [],
             ['CONTENT_TYPE' => 'application/json'],
-            '{}');
+            '{}'
+        );
 
         $this->assertEquals(400, $this->client->getResponse()->getStatusCode());
     }
